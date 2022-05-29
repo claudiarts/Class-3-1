8ab66e9a7b40539fccbd81bcf6978cc901884c11
claudiarts <106489502+claudiarts@users.noreply.github.com>
2022-05-29 18:50:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+temperature=76
+
+if (temperature>75){
+  console.log("It's too hot to walk Olaf for more than 5 minutes")
+} 
+else if(temperature<20){
+  console.log("Please walk Olaf with warm clothes on")
+} else {
+  console.log("Claudia can walk Olaf per usual")
+}
